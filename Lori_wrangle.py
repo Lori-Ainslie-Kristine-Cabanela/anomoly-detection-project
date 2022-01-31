08a0db005b4424d7826da8e2b1dc830ff4532a74
Lori Ainslie <lori.ainslie18@gmail.com>
2022-01-31 12:32:13 -0600

    updating with current progress

--- Lori_wrangle.py
+++ Lori_wrangle.py
@@ -62,3 +62,67 @@ def prep_curriculum_data():
                      'front_end' for x in df.program_id]
     
     return df
+
+
+# create functions to use
+def prep_data_by_user(df, user):
+    '''
+    This function takes in a dataframe and a user id and returns a dataframe with data for only that user
+    '''
+    df = df[df.user_id == user]
+    df.date = pd.to_datetime(df.date)
+    df = df.set_index(df.date)
+    single_user_data = df.path.resample('d').count()
+    return single_user_data
+
+def compute_pct_b(single_user_data, span, weight, user):
+    '''
+    This function adds the %b of a bollinger band range for the page views of a single user's log activity
+    '''
+    # Calculate upper and lower bollinger band
+    midband = single_user_data.ewm(span=span).mean()
+    stdev = single_user_data.ewm(span=span).std()
+    ub = midband + stdev*weight
+    lb = midband - stdev*weight
+    
+    # Add upper and lower band values to dataframe
+    bb = pd.concat([ub, lb], axis=1)
+    
+    # Combine all data into a single dataframe
+    my_df = pd.concat([single_user_data, midband, bb], axis=1)
+    my_df.columns = ['single_user_data', 'midband', 'ub', 'lb']
+    
+    # Calculate percent b and relevant user id to dataframe
+    my_df['pct_b'] = (my_df['single_user_data'] - my_df['lb'])/(my_df['ub'] - my_df['lb'])
+    my_df['user_id'] = user
+    return my_df
+
+def plot_bands(my_df, user):
+    '''
+    This function plots the bollinger bands of the page views for a single user
+    '''
+    fig, ax = plt.subplots(figsize=(12,8))
+    ax.plot(my_df.index, my_df.single_user_data, label='Number of Pages, User: '+str(user))
+    ax.plot(my_df.index, my_df.midband, label = 'EMA/midband')
+    ax.plot(my_df.index, my_df.ub, label = 'Upper Band')
+    ax.plot(my_df.index, my_df.lb, label = 'Lower Band')
+    ax.legend(loc='best')
+    ax.set_ylabel('Number of Pages')
+    plt.show()
+
+def find_anomalies(df, user, span, weight, plot=False):
+    '''
+    This function returns the records where a user's daily activity exceeded the upper limit of a bollinger band range
+    '''
+    # Reduce dataframe to represent a single user
+    single_user_data = prep_data_by_user(df, user)
+    
+    # Add bollinger band data to dataframe
+    my_df = compute_pct_b(single_user_data, span, weight, user)
+    
+     # Plot data if requested (plot=True)
+    if plot:
+        plot_bands(my_df, user)
+    
+    # Return only records that sit outside of bollinger band upper limit
+    return my_df[my_df.pct_b>1]
